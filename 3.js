c0740448d999a4b8726e269f6bd9c852061b8940
mahendra purohit <mahendrapurohit9889@gmail.com>
2023-12-04 09:02:58 +0530

    04-12-2023

---
+++ 3.js
@@ -0,0 +1,146 @@
+        // PROGRAM =1
+
+//  let x=Number(prompt("Enter x: "));
+//  let y=Number(prompt("Enter y: "));
+
+//    if (x<2000 || x>3000) {
+
+//     console.log("velue of x is:" +x);
+
+//    }
+   
+//    if (100<y && y<500) {
+
+//     console.log("velue of x is:" +y);
+
+//    }
+   
+
+
+//    PROGRAM 2 =ENTER TWO VELUEFIND SMOLLEST //
+
+
+    // let x=Number(prompt("Enter x:"));
+    // let y=Number(prompt("Enter y:")); 
+
+    // console.log("smallest velue is =",x<y ? x:y);
+
+
+
+//    PROGRAM 3 =POSITIVE OR NAGETIVE //    
+
+    // let x=Number(prompt("Enter x:"));
+
+    // if(x>0) {
+
+    //     console.log("POSSITIVE");
+    // }
+    // if(x<0) {
+
+    //     console.log("NAGETIVE");
+    // }
+    
+
+//    PROGRAM 4 = DIVISIBLE BY 5 OR NOT  // 
+
+    //    let x=Number(prompt("Enter x:")); 
+
+
+    //    if(x%5==0) {
+
+    //      console.log("DIVISIBLE");
+
+    //    }else 
+
+    //    {
+           
+    //     console.log(" NOT DIVISIBLE");
+    //    }
+
+
+//    PROGRAM 5 = DIVISIBLE BY 5 AND 3 OR NOT  // 
+
+    //  let x=Number(prompt("Enter x:")); 
+
+
+    // if((x%5==0) && (x%3==0)) {
+
+    //  console.log("DIVISIBLE");
+
+    // }else 
+
+    //  {
+       
+    //   console.log("NOT DIVISIBLE");
+
+    // }
+
+//    PROGRAM 6 =  STUDENT PASS OR NOT  //     
+ 
+  
+    //  let GUJ=Number(prompt("Enter GUJ:"));
+    //  let ENG=Number(prompt("Enter ENG :"));
+    //  let SCI=Number(prompt("Enter SCI:"));
+    //  let HIN=Number(prompt("Enter HIN:"));
+    //  let SS=Number(prompt("Enter SS:"));
+    //  let COM=Number(prompt("Enter COM:"));
+    //  let TOTAL;
+    //  let PERCENTAGE;
+
+
+    //   TOTAL=(GUJ+ENG+SCI+HIN+SS+COM);
+
+    //   PERCENTAGE =(TOTAL/600*100);
+      
+    //   console.log("TOTAL=",TOTAL);
+    //   console.log("PERCENTAGE",PERCENTAGE);
+
+    //   if((GUJ<32) || (ENG<32) || (SCI<32) || (SS<32) || (HIN<32) || (COM<32)) {
+
+    //     console.log("FAIL");
+
+    //   }else
+     
+    //   if(PERCENTAGE>33) {
+        
+    //     console.log("PASS");
+    //   }else 
+    //   {
+    //     console.log("FAIL");
+    //   } 
+ 
+//    PROGRAM 7 =  LEAP YEAR OR NOT  //  
+
+    // let x=Number(prompt("Enter x:"));
+
+    // if(x%4==0) {
+  
+    //   console.log(" LEAP YEAR ");
+  
+    // } else {
+
+    //   console.log("NOT A LEAP YEAR");
+
+    // }
+    
+//    PROGRAM 8 =  LARGEST VELUE FIND  //    
+
+    // let x=Number(prompt("Enter x:"));
+    // let y=Number(prompt("Enter y:"));
+    // let z=Number(prompt("Enter z:"));
+
+    // if((x>y) && (x>z)) {
+
+    //     console.log("LARGEST",x);
+    // }
+    // else
+    // if((y>x) && (y>z)) {
+
+    //     console.log("LARGEST",y);
+    // }else
+    // if((z>x) && (z>y)) {
+
+    //     console.log("LARGEST",z);
+    // }
+
+                                   // COMPLETED //
