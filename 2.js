9ac177e12442cfcc7030572fcf8ecb6fd8e6ac54
mahendra purohit <mahendrapurohit9889@gmail.com>
2023-12-06 08:13:02 +0530

    06-12-2023

--- 2.js
+++ 2.js
@@ -33,3 +33,129 @@ typeof
 // console.log(typeof(100 + "a"))
 // console.log(100 * "a")
 // console.log(typeof(100 * "a"))
+
+
+
+
+
+//String => collection of characters (Array)
+
+// let txt;
+
+// let str = "hello Wolrd";
+
+// str = 'Skill Qode'
+
+// txt = "Hello guys'dfszczgd"
+
+// txt = 'Hello guys"dfszczgd'
+
+// console.log(txt);
+
+// String Methods
+
+// // length -> property
+
+// // slice(start position, end position) substring(start position, end position)
+
+// // substr(start position, length)
+
+// // toUpperCase -> upper case convert
+
+// // toLowerCase -> lower case convert
+
+// // charAt -> return index charater
+
+// // charCodeAt -> return index charater ASCII value
+
+// concat -> merge two string
+
+
+
+
+// let data = "Mahendra Purohit "
+
+// console.log(data.length)
+
+// console.log(data.slice(5,12));
+
+// console.log(data.slice(25));
+
+// console.log(data.slice(-12,-3));
+
+// console.log(data.substring(5,12));
+
+// console.log(data.substring(25));
+
+// console.log(data.substring(-12));
+
+// console.log(data.substring(-12,5));
+
+// console.log(data.substr(5,12));
+
+// console.log(data.substr(-15,12));
+
+//console.log(data.toUpperCase());
+
+// console.log(data.toLowerCase());
+
+// console.log(data.charAt(5));
+
+// console.log(data.charCodeAt(0));
+// console.log(data.charCodeAt(1));
+// console.log(data.charCodeAt(5));
+
+// cript i
+
+// starting to rest of all string
+
+// last to counting
+
+// cript i
+
+// starting to rest of all string
+
+// counting to 0
+
+// counting to
+
+// cript is the
+
+//mming langua
+
+// console.log(data.concat("Hello Wolrd", "\n SkillQode"));
+
+
+
+
+let data = "javascript is the world's most popullar laguage in the world ";
+
+ console.log(data)
+//  console.log(data.replace("worlds","india"))
+// console.log(data.replace(/Worlds/i , "india" ))
+// console.log(data.replace(/worlds/m , "india" ))
+// console.log(data.replace(/worlds/g , "india" ))   
+
+// console.log(data.replaceAll("world" , "india"))
+
+// let txt = "        hello world        ";
+
+// console.log(txt);
+// console.log(txt.trim());     // remove extra space 
+// console.log(txt.trimStart());     // remove extra space frome start
+// console.log(txt.trimEnd());     // remove extra space frome end
+// console.log("Lenght is :" + txt.trimEnd().length);     
+
+// let txt = "98434";
+// console.log(txt.padStart(8 , "X"))
+// console.log(txt.padEnd(8 , "X"))
+
+
+// console.log(data.split(""))
+// console.log(data.split(" "))
+// console.log(data.split("a"))
+
+
+   console.log(data.split(""))
+
+
--- 4.js
+++ 4.js
@@ -80,4 +80,4 @@
 
     // }
 
-                                     //* COMPLETED //*
+                                  //* COMPLETED //*
