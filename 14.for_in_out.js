b643006b37041c5a631e58a9083a46db2bda1e48
Mahendra purohit <Mahendrapurohitmewada@gmail.com>
2023-12-22 09:56:13 +0530

    22-12-2023

--- 14.for_in_out.js
+++ 14.for_in_out.js
@@ -26,17 +26,17 @@
 
 // ... rest parameter / spread operator
 
-let person = {
-    name: "Skill",
-    age: 25,
-    email: "skill@test.in",
-  };
+// let person = {  
+//     name: "Skill",
+//     age: 25,
+//     email: "skill@test.in",
+//   };
   
   // let data = person;
-  let data = { ...person };
-  
-  person.salary = 1500;
-  
-  data.hobbies = ["music", "dancing"];
-  console.log(person);
-  console.log(data);
+  // let data = { ...person };
+  // 
+  // person.salary = 1500;
+  // 
+  // data.hobbies = ["music", "dancing"];
+  // console.log(person);
+  // console.log(data);
